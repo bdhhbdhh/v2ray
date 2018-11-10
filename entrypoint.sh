1eb7533b6baccb11207449884bcd6f168e56a9ac
bdhhbdhh <44903340+bdhhbdhh@users.noreply.github.com>
2018-11-10 18:15:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
-mkdir /etc/v2ray
-echo -e "$CONFIG" > /etc/v2ray/config.json
-v2ray -config=/etc/v2ray/config.json
+echo -e "$CONFIG" > /usr/bin/v2ray/config.json
+v2ray -config=/usr/bin/v2ray/config.json
